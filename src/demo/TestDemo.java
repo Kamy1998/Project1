62616745a92c94595f17781acb2c96137ea524b6
Kamy <Kamy@4Ever.mynetworksettings.com>
2024-01-07 15:44:11 -0500

    Initial Commit

---
+++ src/demo/TestDemo.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+
+		System.out.println("I love Java");
+		
+		System.out.println("Not so much tho...");
+
+	}
+
+}
